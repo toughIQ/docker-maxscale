889a4089c14ee23cf05a6e241db9b26a0109f978
Christoph Mertins <c.mertins@gmail.com>
2021-01-06 10:49:23 +0100

    xargs

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKEND_SERVER_LIST=`/getbackendservers.sh`
+BACKEND_SERVER_LIST=`/getbackendservers.sh|xargs`
 CONFIGURED_SERVER_LIST=`cat /tmp/configuredbackservers|xargs`
 
 if [[ "$BACKEND_SERVER_LIST" == "$CONFIGURED_SERVER_LIST" ]]
